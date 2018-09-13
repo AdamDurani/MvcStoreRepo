811322962c36ca11bab6f38a67204a231c46d8e9
TOMBOLAEMEA\adam.durani <adam.durani@tombola.com>
2018-09-13 10:03:18 +0100

    Initial commit

---
+++ MVCStore/MVCStore/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCStore.Models
+{
+    public class Movie
+    {
+        public enum GenreType
+        {
+            Horror, Adventure, Action, Thriller
+        }
+
+        public byte Id { get; set; }
+        public String Title { get; set; }
+        public GenreType Genre { get; set; }
+        public int DateOfRelease { get; set; }
+        public double Price { get; set; }
+
+
+        
+    }
+}
---
+++ MVCStore/MVCStore/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCStore.Models
+{
+    public class Person
+    {
+        public byte Id { get; set; }
+        public String FirstName { get; set; }
+        public String SecondName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        
+    }
+}
